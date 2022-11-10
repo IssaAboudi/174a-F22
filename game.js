1a82d7f6727939d18f06c6c432932940f544982d
IssaLaptop <ma42202@hotmail.com>
2022-11-10 12:44:15 -0800

    moved camera and fixed camera to see the level, changed inputs to only control paddle (WIP)

--- game.js
+++ game.js
@@ -1,26 +1,12 @@
 import { defs, tiny } from "./examples/common.js";
+// import { Keyboard_Manager } from "./tiny-graphics.js";
 
 // Modified tiny-graphics-widgets.js common.js
 // to clean up game webpage (disabled code block widget, file structure widget,
 // and basic movement controls set by default - we will create our own movement controls
 // called "Game_Controls")
 
-const {
-  Vector,
-  Vector3,
-  vec,
-  vec3,
-  vec4,
-  color,
-  hex_color,
-  Shader,
-  Matrix,
-  Mat4,
-  Light,
-  Shape,
-  Material,
-  Scene,
-} = tiny;
+const { Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene } = tiny;
 
 class Cube extends Shape {
   constructor() {
@@ -79,44 +65,9 @@ class Cube extends Shape {
       [0, 0, -1]
     );
     // Arrange the vertices into a square shape in texture space too:
-    this.indices.push(
-      0,
-      1,
-      2,
-      1,
-      3,
-      2,
-      4,
-      5,
-      6,
-      5,
-      7,
-      6,
-      8,
-      9,
-      10,
-      9,
-      11,
-      10,
-      12,
-      13,
-      14,
-      13,
-      15,
-      14,
-      16,
-      17,
-      18,
-      17,
-      19,
-      18,
-      20,
-      21,
-      22,
-      21,
-      23,
-      22
-    );
+    this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6,
+                       8, 9, 10, 9, 11, 10, 12, 13, 14, 13, 15, 14,
+                       16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
   }
 }
 
@@ -148,13 +99,14 @@ const Game_Controls = (defs.Game_Controls = class Game_Controls extends Scene {
   constructor() {
     super();
     const data_members = {
-      thrust: new Vector(0, 0),
+      thrust: new Vector(0, 0), //we only need movement in X axis for the paddle
       pos: vec3(0, 0, 0),
       z_axis: vec3(0, 0, 0),
       radians_per_frame: 1 / 200,
       meters_per_frame: 10,
       speed_multiplier: 1,
     };
+
     Object.assign(this, data_members);
 
     this.mouse_enabled_canvases = new Set();
@@ -182,19 +134,12 @@ const Game_Controls = (defs.Game_Controls = class Game_Controls extends Scene {
     //Make all the buttons for in the control panel here:
 
     // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-    this.key_triggered_button(
-      "Sample Button",
-      ["A"],
-      () => (this.attached = () => null)
-    );
+    this.key_triggered_button("Move Paddle Left", ["a"], () => global_transform.add(Mat4.translation(5,0,0)),   undefined, () => global_transform.add(Mat4.translation(-5,0,0)));
+    this.key_triggered_button("Move Paddle Right", ["d"], () => global_transform.add(Mat4.translation(-5,0,0)), undefined, () => global_transform.add(Mat4.translation(5,0,0)));
     this.new_line();
   }
 
-  display(
-    context,
-    graphics_state,
-    dt = graphics_state.animation_delta_time / 1000
-  ) {
+  display(context, graphics_state, dt = graphics_state.animation_delta_time / 1000) {
     // The whole process of acting upon controls begins here.
     const m = this.speed_multiplier * this.meters_per_frame,
       r = this.speed_multiplier * this.radians_per_frame;
@@ -205,6 +150,8 @@ const Game_Controls = (defs.Game_Controls = class Game_Controls extends Scene {
     }
   }
 });
+
+let global_transform = new Mat4(5,0,0);
 
 export class BrickBreaker extends Scene {
   constructor() {
@@ -220,34 +167,27 @@ export class BrickBreaker extends Scene {
 
     this.materials = {
       default: new Material(new defs.Phong_Shader(), {
-        ambient: 0.4,
-        diffusivity: 0.6,
-        color: hex_color("#ffffff"),
+          ambient: 0.4, diffusivity: 0.6, color: hex_color("#ffffff"),
       }),
       plastic: new Material(new defs.Phong_Shader(), {
-        ambient: 1,
-        diffusivity: 1,
-        specularity: 1,
-        color: hex_color("#b08040"),
+          ambient: 1, diffusivity: 1, specularity: 1, color: hex_color("#b08040"),
       }),
     };
 
     this.white = new Material(new defs.Basic_Shader());
 
-    this.initial_camera_location = Mat4.look_at(
-      vec3(0, 10, 20),
-      vec3(0, 0, 0),
-      vec3(0, 1, 0)
-    );
+    let eye_position = vec3(30,20,60); //Initialize Camera to
+    let eye_look_at = vec3(30,20,0); //Set camera to look at the middle of the game window
+
+    this.initial_camera_location = Mat4.look_at(eye_position, eye_look_at, vec3(0, 1, 0));
 
     this.colors = [];
     this.set_colors();
   }
 
   set_colors() {
-    // TODO:  Create a class member variable to store your cube's colors.
-    // Hint:  You might need to create a member variable at somewhere to store the colors, using `this`.
-    // Hint2: You can consider add a constructor for class Assignment2, or add member variables in Base_Scene's constructor.
+
+
     this.colors = [];
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
@@ -264,81 +204,70 @@ export class BrickBreaker extends Scene {
     // display():  Called once per frame of animation.
     // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
     if (!context.scratchpad.controls) {
-      this.children.push(
-        (context.scratchpad.controls = new defs.Movement_Controls())
-      );
+      this.children.push((context.scratchpad.controls = new defs.Game_Controls()));
+      // this.children.push((context.scratchpad.controls = new defs.Movement_Controls())); //Don't need this because camera is fixed
+
       // Define the global camera and projection matrices, which are stored in program_state.
       program_state.set_camera(this.initial_camera_location);
     }
 
-    program_state.projection_transform = Mat4.perspective(
-      Math.PI / 4,
-      context.width / context.height,
-      0.1,
-      1000
-    );
-
-    let axis_model_transform = Mat4.identity();
-    this.shapes.axis.draw(
-      context,
-      program_state,
-      axis_model_transform,
-      this.white,
-      "LINES"
-    );
+    program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 0.1, 1000);
 
     const ts = program_state.animation_time / 1000; //time step (for each second of animation)
     const dt = program_state.animation_delta_time / 1000; //time difference between current and last frame (keep game frame independent)
 
-    //add code here
-    program_state.lights = [
-      new Light(vec4(0, 0, 20, 1), color(1, 1, 1, 1), 10 ** 10),
-    ]; //Default Light
+
+    program_state.lights = [new Light(vec4(0, 0, 20, 1), color(1, 1, 1, 1), 10 ** 10),]; //Default Lighting for project
+
+    // let axis_model_transform = Mat4.identity();
+    // this.shapes.axis.draw(context, program_state, axis_model_transform, this.white, "LINES");
+
+    let movement_factor = 2; //how much we translate the paddle when we press the key
 
     let brick_transform = Mat4.translation(0, 20, 0);
-    brick_transform = Mat4.translation(3, 0, 0).times(
-      Mat4.scale(3, 1, 1).times(brick_transform)
-    );
+    brick_transform = Mat4.translation(3, 0, 0)
+        .times(Mat4.scale(3, 1, 1).times(brick_transform))
+        ;
 
     // Draw Cube Grid 10x 10 (use function to set size this is temp)
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
-        this.shapes.bricks.draw(
-          context,
-          program_state,
-          Mat4.translation(6.4 * j, 2.4 * i, 0).times(brick_transform),
-          this.materials.plastic.override({ color: this.colors[i + i * j] })
-        );
+        this.shapes.bricks.draw(context, program_state, Mat4.translation(6.4 * j, 2.4 * i, 0).times(brick_transform),
+            this.materials.plastic.override({ color: this.colors[i + i * j] }));
       }
     }
 
-    // Draw plate (centered it, but use variables to keep it always centered
-    // depending on grid pramaeters) (use a deformed sphere so that the ball goes
-    // off at different angles)
-    let plate_transform = Mat4.identity();
-    plate_transform = Mat4.translation(1 * 6.2 + 4 * 6.4, 0, 0).times(
-      Mat4.scale(6.2, 2, 1).times(plate_transform)
-    );
+    // Draw paddle (centered it, but use variables to keep it always centered depending on grid pramaeters)
+    // (use a deformed sphere so that the ball goes off at different angles)
+      // ~ Changes 11/10/2022 by Issa: Changed name from plate_transform to paddle_transform (more descriptive)
+    let paddle_transform = Mat4.identity(); //this is redundant
+    paddle_transform = paddle_transform.times(Mat4.translation(1 * 6.2 + 4 * 6.4, 0, 0)
+        .times(Mat4.scale(6.2, 2, 1)
+        .times(paddle_transform)))
+    ;
 
-    this.shapes.sphere.draw(
-      context,
-      program_state,
-      plate_transform,
-      this.materials.plastic
-    );
+    // document.addEventListener("Onkeydown", function(event){
+    //   if(event.target == a.keyCode){
+    //     console.log("a was pressed");
+    //     paddle_transform = paddle_transform.add(Mat4.translation(5,0,0));
+    //   }
+    // });
+    //
+    // document.addEventListener("Onkeydown", function(event) {
+    //   if(event.target == d.keyCode){
+    //     console.log("d was pressed");
+    //     paddle_transform = paddle_transform.add(Mat4.translation(-5,0,0));
+    //   }
+    // });
+
+    this.shapes.sphere.draw(context, program_state, paddle_transform, this.materials.plastic);
 
     // Draw ball
     let ball_transform = Mat4.identity();
-    ball_transform = Mat4.translation(1 * 6.2 + 4 * 6.4, 1 + 2, 0).times(
-      ball_transform
-    );
+    ball_transform = Mat4.translation(1 * 6.2 + 4 * 6.4, 1 + 2, 0)
+        .times(ball_transform);
 
-    this.shapes.sphere.draw(
-      context,
-      program_state,
-      ball_transform,
-      this.materials.default
-    );
+    this.shapes.sphere.draw(context, program_state, ball_transform, this.materials.default);
   }
 }
 
